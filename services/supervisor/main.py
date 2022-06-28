ed7a85dd15aa06b5c059f938f5d4fa37b4d1b738
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-06-28 18:30:26 -0300

    Restart if unreachable for too long
    Before that, any unsucess reaching the service led to restarting it.

--- services/supervisor/main.py
+++ services/supervisor/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from collections import namedtuple
+from datetime import datetime
 from os import system
 import time
 from typing import List
@@ -14,13 +15,14 @@ SERVICES_PATH=f"{SOLAR_SURFER2}/services"
 TOOLS_PATH=f"{SOLAR_SURFER2}/tools"
 LOGS_PATH="/var/logs/blueos/solarsurfer"
 
-Service = namedtuple("Service", ["name", "status_url", "seconds_off_before_killing", "command_line"])
+Service = namedtuple("Service", ["name", "status_url", "seconds_off_before_killing", "command_line", "utc_time_last_reach"])
 services: List[Service] = [
     Service(
         name="sats_comm",
         status_url="http://127.0.0.1:9992/status",
         seconds_off_before_killing=3600,
         command_line=f"{SERVICES_PATH}/sats_comm/main.py --serial /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-port0 --loguru-output-dir {LOGS_PATH}/sats_comm --verbosity DEBUG",
+        utc_time_last_reach=datetime.utcnow(),
     ),
 ]
 
@@ -45,21 +47,24 @@ def main():
         logger.debug(f"Resting for {REST_TIME_SERVICES_CHECK} seconds before next supervisor cycle.")
         time.sleep(REST_TIME_SERVICES_CHECK)
         for service in services:
-            restart = False
+            unreachable_for_too_long = False
+            no_heartbeats_for_too_long = False
             try:
                 logger.info(f"Checking status of '{service.name}' service.")
                 response = requests.get(service.status_url, timeout=10.0)
                 data = response.json()
                 seconds_since_last_heartbeat = data["secondsSinceLastHeartbeat"]
+                service.utc_time_last_reach = datetime.utcnow()
                 logger.debug(f"Seconds since last '{service.name}' heartbeat: {seconds_since_last_heartbeat}.")
-                if seconds_since_last_heartbeat > service.seconds_off_before_killing:
-                    restart = True
+                no_heartbeats_for_too_long = seconds_since_last_heartbeat > service.seconds_off_before_killing
             except Exception as error:
                 logger.error("Could not check satellite loop status.")
                 logger.exception(error)
-                restart = True
             finally:
-                if restart:
+                seconds_since_last_reachable = (datetime.utcnow() - service.utc_time_last_reach).seconds
+                logger.debug(f"Seconds since last reach on '{service.name}': {seconds_since_last_reachable}.")
+                unreachable_for_too_long = seconds_since_last_reachable > service.seconds_off_before_killing
+                if no_heartbeats_for_too_long or unreachable_for_too_long:
                     try:
                         restart_service(service)
                     except Exception as error:
